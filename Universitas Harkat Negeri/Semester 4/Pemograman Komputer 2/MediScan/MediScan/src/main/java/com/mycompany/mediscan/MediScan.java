ac0d6d39be4503faa11e69099b28138f3e08939e
SYAMSUL00 <samsulhidayah384@gmail.com>
2026-04-23 00:06:45 +0700

    Upload file Entity Class MediScan.

---
+++ Universitas Harkat Negeri/Semester 4/Pemograman Komputer 2/MediScan/MediScan/src/main/java/com/mycompany/mediscan/MediScan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mediscan;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class MediScan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
